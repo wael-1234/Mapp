91bb6cc28340c4f1b0db9cb5bab6f1c1778e2aa3
wael <ben.abderrahmen.wael@gmail.com>
2023-03-07 00:35:56 +0100

    Map adding

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,35 @@
-import react from 'react';
+import react, {useState} from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import makeStyles from "./styles";
 
 const List = () => {
+
+    const classes = makeStyles();
+    
+    const [type, setType]=useState('Restaurant');
+    const [rating,setRating]=useState('');
+    
     return(
-        <h1> List</h1>
+        <div className={classes.container}>
+            <Typography variant='h4'> Restaurants, Hotels & Attractions araound you ...</Typography>
+            <FormControl className={classes.FormControl}>
+                <InputLabel>Type</InputLabel>
+                <Select value={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value='restaurants'>Restaurants</MenuItem>
+                    <MenuItem value='hotels'>Hotels</MenuItem>
+                    <MenuItem value='attractions'>Attractions</MenuItem>
+                </Select>
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={1}>Above 1</MenuItem>
+                    <MenuItem value={3}>Above 3</MenuItem>
+                    <MenuItem value={4}>Above 4</MenuItem>
+                    <MenuItem value={4.5}>Above 4.5</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
     )
 }
 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,40 @@
-import react from 'react';
+import react from "react";
+import googleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { LocationOnOutlined } from "@material-ui/icons/LocationOnOutlined";
+import { Rating } from "@material-ui/lab";
+import useStyles from "./styles";
 
 const Map = () => {
-    return(
-        <h1>Map</h1>
-    )
-}
+  const classes = useStyles();
+  const isMobile = useMediaQuery("min-width:600px");
+  const coordinates = { Lat: 0, Lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <googleMapReact
+        bootstrapurlkeys={{ key: "" }}
+        defaultcenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      >
+        <iframe
+          margin="200px"
+          width="103%"
+          height="102%"
+          id="gmap_canvas"
+          src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        ></iframe>
+      </googleMapReact>
+    </div>
+  );
+};
 
 export default Map;
