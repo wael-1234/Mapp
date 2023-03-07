a3518a2338fef24c41b3a6b8d5b4596c184dfeda
wael <ben.abderrahmen.wael@gmail.com>
2023-03-07 01:35:26 +0100

    map adding

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -7,28 +7,29 @@ const List = () => {
 
     const classes = makeStyles();
     
-    const [type, setType]=useState('Restaurant');
+    const [type, setType]=useState('');
     const [rating,setRating]=useState('');
     
     return(
         <div className={classes.container}>
             <Typography variant='h4'> Restaurants, Hotels & Attractions araound you ...</Typography>
-            <FormControl className={classes.FormControl}>
-                <InputLabel>Type</InputLabel>
-                <Select value={type} onChange={(e) => setType(e.target.value)}>
-                    <MenuItem value='restaurants'>Restaurants</MenuItem>
-                    <MenuItem value='hotels'>Hotels</MenuItem>
-                    <MenuItem value='attractions'>Attractions</MenuItem>
-                </Select>
-                <InputLabel>Rating</InputLabel>
-                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
-                    <MenuItem value={0}>All</MenuItem>
-                    <MenuItem value={1}>Above 1</MenuItem>
-                    <MenuItem value={3}>Above 3</MenuItem>
-                    <MenuItem value={4}>Above 4</MenuItem>
-                    <MenuItem value={4.5}>Above 4.5</MenuItem>
-                </Select>
-            </FormControl>
+            <FormControl className={classes.formControl}>
+            <InputLabel id="type">Type</InputLabel>
+            <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="hotels">Hotels</MenuItem>
+              <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="rating">Rating</InputLabel>
+            <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
+              <MenuItem value="">All</MenuItem>
+              <MenuItem value="3">Above 3.0</MenuItem>
+              <MenuItem value="4">Above 4.0</MenuItem>
+              <MenuItem value="4.5">Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
         </div>
     )
 }
